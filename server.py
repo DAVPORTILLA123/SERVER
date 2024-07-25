bb1b7ba6b47dc5d6b2bb837a18254f593e0f7b67
DAVPORTILLA123 <portillajuanda123@gmail.com>
2024-07-25 02:25:21 +0000

    Mundo guardado 2024-07-25

--- server.py
+++ server.py
@@ -17,16 +17,11 @@
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (Californ
 # 
-
-
-
-
-
-
-# 
-
-
 #
+
+
+
+
 #  nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
